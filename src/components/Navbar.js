3941afaa73576656e5ae6a4b642462908504091d
Debbie-alt <115513115+Debbie-alt@users.noreply.github.com>
2024-05-08 22:23:33 +0100

    Fix::Mobile Design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
        <nav className='w-screen  p-4 md:p-8 px-5  md:px-10 flex justify-between'>
             <h1  className=" text-lg md:text-2xl font-semibold text-orange-800 tracking-wide">ForeCastify</h1>
-            <div className=' space-x-3 md:space-x-5 text-xs md:text-md'> 
+            <div className=' space-x-3 md:space-x-5 text-xs md:text-lg'> 
               <Link href='/' className='hover:text-orange-900 hover:opacity-80'>Home</Link>
                   <Link href='/about' className='hover:text-orange-900 hover:opacity-80'>About</Link>
                   <Link href='/about'  className='hover:text-orange-900 hover:opacity-80'>Services</Link>
