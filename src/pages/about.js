6a177fdb3dc802db13703ef30ff5cc0ce218510f
Debbie-alt <115513115+Debbie-alt@users.noreply.github.com>
2024-05-08 21:43:40 +0100

    Update: About Router

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,9 @@ const about = () => {
   return (
             <>
             <Navbar/>
+                 <div className="w-screen bg-red-500 flex flex-col items-center">
+                     <h1 className="font-semibold text-center text-3xl">About Forecastify</h1>
+                 </div>
             </>
   );
 };
